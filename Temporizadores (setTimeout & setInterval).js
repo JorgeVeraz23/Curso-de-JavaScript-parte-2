3c222d80cc2e13fd8de63f8b889dc45671a6213a
JorgeVeraz23 <JorgeVeraZambrano@outlook.com>
2023-07-24 16:24:46 -0500

    first commit

---
+++ Temporizadores (setTimeout & setInterval).js
@@ -0,0 +1,27 @@
+/*console.log("inicio");
+setTimeout(() => {
+    console.log("Ejecutando un setTimeOut esto se ejecuta una sola vez");
+}, 3000);
+
+setInterval(() => {
+    console.log("Ejecutando un setInterval, esto se ejecuta indefinidamente cada cierto intervalo de tiempo");
+}, 1000);*/
+/*
+setInterval(() => {
+    console.log(new Date().toLocaleTimeString());
+}, 1000);*/
+/*
+let temporizador = setTimeout(() => {
+    console.log(new Date().toLocaleTimeString());
+}, 1000);
+
+clearTimeout(temporizador);
+console.log("Despues del clearTimeout")
+*/
+
+let temporizador = setInterval(() => {
+    console.log(new Date().toLocaleTimeString());
+}, 1000);
+
+clearInterval(temporizador);
+console.log("Despues del clearInterval");
